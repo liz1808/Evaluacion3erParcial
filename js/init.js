6195338582439d3339626dbd5e2e526ddb7027ea
liz1808 <81264170+liz1808@users.noreply.github.com>
2021-04-06 12:54:41 -0500

    Update init.js

--- js/init.js
+++ js/init.js
@@ -1,12 +1,4 @@
-/*  Inicializa Firebase con la
- * configuración del proyecto.
- * Revisa la configuración en tu
- * servidor de Firebase y cópiala
- * aquí sustituyendo los
- * asteriscos. Los campos deben
- * quedar igual que en tu
- * servidor. */
-// @ts-ignore
+
 firebase.initializeApp({
     apiKey: "AIzaSyDTORhF11V8eCGH07xcgfStMM3VzLa-xtk",
     authDomain: "evaluacion3erparcial.firebaseapp.com",
