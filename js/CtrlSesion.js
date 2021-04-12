7effb0a067b7f8885d79b0217c16772b2fe70bd3
liz1808 <81264170+liz1808@users.noreply.github.com>
2021-04-12 13:28:30 -0500

    Update CtrlSesion.js

--- js/CtrlSesion.js
+++ js/CtrlSesion.js
@@ -15,15 +15,33 @@ const forma = document["forma"];
 const avatar = document.
   querySelector("#avatar");
 
-
+/* Escucha cambios de usuario.
+ * El primer parámetro es una
+ * función que se invoca cada que
+ * hay un cambio de usuario y
+ * recibe los datos del usuario.
+ * El segundo parámetro es una
+ * función que se invoca cuando se
+ * presenta un error en un cambio
+ * de usuario y recibe un Error.
+ */
 getAuth().onAuthStateChanged(
   muestraSesión, muestraError);
 
-
+/** Muestra los datos del usuario
+ * o manda a iniciar sesión en
+ * caso de que no haya empezado.
+ * @param {import(
+    "../lib/tiposFire").
+    User} usuario modelo con las
+ *    características del usuario
+ *    o null si no ha iniciado
+ *    sesión. */
 async function
   muestraSesión(usuario) {
   if (usuario && usuario.email) {
-    
+    // Usuario aceptado.
+    console.log(usuario)
     forma.email.value =
       usuario.email || "";
     forma.nombre.value =
@@ -34,7 +52,7 @@ async function
       addEventListener(
         "click", terminaSesión);
   } else {
-    
+    // No ha iniciado sesión.
     iniciaSesión();
   }
 }
